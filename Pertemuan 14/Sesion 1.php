3412314773c4faaf1617ffd3081c1363e1ae6b67
sandro <samuelgelardo@gmail.com>
2026-05-29 16:53:27 +0700

    Update Sesion 1.php

--- Pertemuan 14/Sesion 1.php
+++ Pertemuan 14/Sesion 1.php
@@ -1,34 +1,57 @@
 <?php
 /****************************************************
-Halaman ini merupakan halaman contoh penciptaan session. Perintah session_start() harus ditaruh di perintah pertama tanpa spasi di depannya. Perintah session_start() harus ada pada setiap halaman yang berhubungan dengan session
+ Halaman Login dengan Session
 *****************************************************/
+
 session_start();
-if (isset ($_POST['Login'])) {
-$user = $_POST['user'];
-$pass = $_POST['pass'];
-  //periksa login
-if ($user == "rahadian" && $pass = "123") {
-//menciptakan session
-$_SESSION['login'] = $user;
-//menuju ke halaman pemeriksaan session
-echo "<h1>Anda berhasil LOGIN</h1>";
-echo "<h2>Klik <a href='session2.php'>di sini
-(session2.php)</a>
-untuk menuju ke halaman pemeriksaan session";
+
+// Cek apakah tombol login ditekan
+if (isset($_POST['Login'])) {
+
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
+
+    // Periksa login
+    if ($user == "rahadian" && $pass == "123") {
+
+        // Membuat session
+        $_SESSION['login'] = $user;
+
+        echo "<h1>Anda berhasil LOGIN</h1>";
+        echo "<h2>Klik 
+        <a href='session2.php'>di sini (session2.php)</a> 
+        untuk menuju ke halaman pemeriksaan session</h2>";
+
+    } else {
+
+        echo "<h2>Username atau Password salah!</h2>";
+    }
 }
-} else {
 ?>
+
+<!DOCTYPE html>
 <html>
 <head>
-<title>Login here...</title>
+    <title>Login Here...</title>
 </head>
+
 <body>
+
 <form action="" method="post">
-<h2>Login Here...</h2>
-Username : <input type="text" name="user"><br>
-Password : <input type="password" name="pass"><br>
-<input type="submit" name="Login" value="Log In">
+
+    <h2>Login Here...</h2>
+
+    Username :
+    <input type="text" name="user" required>
+    <br><br>
+
+    Password :
+    <input type="password" name="pass" required>
+    <br><br>
+
+    <input type="submit" name="Login" value="Log In">
+
 </form>
+
 </body>
 </html>
-  
