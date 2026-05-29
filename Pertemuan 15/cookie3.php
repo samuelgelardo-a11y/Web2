cd45fde553bae2f0310b1205818ae14f8de6173b
sandro <samuelgelardo@gmail.com>
2026-05-29 18:44:41 +0700

    Create cookie3.php

---
+++ Pertemuan 15/cookie3.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Menghapus Cookies
+|--------------------------------------------------------------------------
+*/
+
+// Menghapus cookie dengan waktu kadaluarsa yang sudah lewat
+setcookie("username", "", time() - 3600);
+
+setcookie("namalengkap", "", time() - 3600);
+
+
+echo "<h1>Cookie berhasil dihapus.</h1>";
+
+echo "<h2>
+Klik <a href='cookie1.php'>di sini</a>
+untuk penciptaan cookies
+</h2>";
+
+echo "<h2>
+Klik <a href='cookie2.php'>di sini</a>
+untuk pemeriksaan cookies
+</h2>";
+
+?>
